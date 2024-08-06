cc23edab26bf9b0b0d2b2efbfa521c20ae133821
Vinayak <vinayakpawarinstance@gmail.com>
2024-08-06 18:24:15 +0530

    episode 03

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "i am H1 tag"),
-        React.createElement("h1", {}, "i am h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am child2 h1"),
-        React.createElement("h2", {}, "i am child2 h2"),
-    ]),
-]);
- 
+//jsx
+// react Element
+const heading = (
+    <h1 id="heading" className="vins" tabIndex="5">
+        this is jsxHeading
+    </h1>
+);
+console.log(heading); // object
+
+// react Component
+const HeadingComponet = () => {
+    return <h1>functional component</h1>;
+};
+
+const Title = () => <h1 id="heading">Title Component....!</h1>;
+
+const NormalFunctionComponent = function () {
+    return <h1 id="heading">I am a normal function component</h1>;
+};
+const HeadingComponent2 = () => (
+    <div id="container">
+        <Title />
+        <HeadingComponet />
+        <NormalFunctionComponent />
+        <h1 className="heading">functional component 2 </h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
- 
-root.render(parent);
+
+root.render(<HeadingComponent2 />);
